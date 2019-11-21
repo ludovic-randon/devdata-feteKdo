dea37ac43594387525996a65a9edf0623e02c90e
Ludo-R <ludovic.randon.data@gmail.com>
2019-11-21 09:45:32 +0100

    First Commit

---
+++ fetekdo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 08:58:58 2019
+
+@author: ludodata
+"""
+
+import function as func
+
+
+choiceMenu = input("Menu:\n\n 1. FETEKDO\n\n 2. ReadMe\n\n 3. Quitter \n\n")
+
+while choiceMenu == "1" or "2":
+    if choiceMenu == "1":
+        func.fetekdo()
+        choiceMenu = input("Menu:\n\n 1. FETEKDO\n\n 2. ReadMe\n\n 3. Quitter \n")
+
+    elif choiceMenu == "2":
+        print("REEEAAAADMEEE")
+        choiceMenu = input("Menu:\n\n 1. FETEKDO\n\n 2. ReadMe\n\n 3. Quitter \n")
+
+    else: 
+        break
---
+++ function.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 08:58:25 2019
+
+@author: ludodata
+"""
+
+import random
+import pandas
+
+
+def fetekdo():
+    df = pandas.read_csv('/Users/ludodata/Simplon/PROJECT/FETEKDO/names.csv')
+    list_csv = df.values.tolist()
+        
+    list = []
+    for i in list_csv:
+        list = list+i
+    
+    random.shuffle(list)
+
+    i=0
+
+    while i != len(list):
+        index_max = len(list) - 1
+        continue_sort = input('Tirer le duo suivant ? O/N ')
+        if continue_sort.lower() == 'o':
+            if i < index_max:
+                print(list[i], ' donne à ', list[i+1])
+                i = i + 1
+            else:
+                print(list[len(list) - 1], ' donne à ', list[0])
+                break
+        elif continue_sort.lower() == 'n':
+            print('Salut, à la prochaine !')
+            break
+        else:
+            print("Lettre 'O' ou lettre 'N' !")
