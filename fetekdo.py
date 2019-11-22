c871a0818e0b71cd69962fa10af7e6eeed673155
Ludo-R <ludovic.randon.data@gmail.com>
2019-11-22 16:24:40 +0100

    modif certain label, adding img, return bg > white

--- fetekdo.py
+++ fetekdo.py
@@ -9,6 +9,8 @@ Created on Thu Nov 21 08:58:58 2019
 import random
 import pandas
 from tkinter import *
+from PIL import ImageTk, Image
+import os
 
 
 def readme():
@@ -23,16 +25,16 @@ def fetekdo():
     for i in list_csv:
         list = list+i
     random.shuffle(list)
-    duo_sort = Label(text = 'Le premier : ', bg = "#B4B7C7")
+    duo_sort = Label(text = 'Le premier : ')
     duo_sort.pack()
     for i in list:
-        duo_sort = Label(text = i + ' donne à :', bg = "#B4B7C7")
+        duo_sort = Label(text = i + ' donne à :')
         duo_sort.pack()
-    duo_sort = Label(text = list[0] + ', fin de la liste !', bg = "#B4B7C7")
+    duo_sort = Label(text = list[0] + ', fin de la liste !')
     duo_sort.pack()
 
 def funFetekdo():
-    nextDuo = Button(mainWindow, text = "Afficher le duo suivant", command = fetekdo)
+    nextDuo = Button(mainWindow, text = "Afficher la liste aléatoire", fg="red", command = fetekdo)
     nextDuo.pack(side = TOP, ipadx = 40, padx = 40, ipady = 20, pady = 40)
     choiceMenu1.pack_forget()
     choiceMenu2.pack_forget()
@@ -41,13 +43,15 @@ def readme():
     wReadme = Toplevel(mainWindow)
 
 mainWindow = Tk()
+img = ImageTk.PhotoImage(master = mainWindow, file="kdo.gif")
+panel = Label(mainWindow, image = img)
+panel.pack(side = "left", fill = "both", expand = "yes")
 
 mainWindow.title('FETEKDO')
-mainWindow.configure(bg = "#B4B7C7")
 
 choiceMenu1 = Button(mainWindow, text = "FETEKDO", fg="red", command = funFetekdo)
 choiceMenu1.pack(side = TOP, ipadx = 20, padx = 60, ipady = 25, pady = 25)
-choiceMenu2 = Button(mainWindow, text = "ReadMe", fg="red", command = readme)
+choiceMenu2 = Button(mainWindow, text = "Choisir Fichier", fg="red", command = readme)
 choiceMenu2.pack(side = TOP, ipadx = 20, padx = 60, ipady = 25, pady = 25)
 choiceMenu3 = Button(mainWindow, text = "Quitter", fg="black", command = mainWindow.destroy)
 choiceMenu3.pack(side = TOP, ipadx = 20, padx = 60, ipady = 25, pady = 15)
